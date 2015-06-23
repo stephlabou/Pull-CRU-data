ba129242ccfb48256bf9b2a603554d17484fec29
Kara Woo <woo.kara@gmail.com>
2015-06-22 17:24:51 -0700

    Clarify a comment

--- CruPrecipFull.R
+++ CruPrecipFull.R
@@ -108,7 +108,7 @@ end.month.adj <- vector(mode="integer", length=2271)
 
 full_adj <- full %>% 
   mutate(end.year.adj = ifelse(end.month.num >= 12, endyear + 1, endyear), #if December, goes to Jan of following year
-         end.month.adj = ifelse(end.month.num >= 12, 1, end.month.num+1)) #if not Dec, kicks forward one month
+         end.month.adj = ifelse(end.month.num >= 12, 1, end.month.num+1)) # if Dec, make it Jan, otherwise if not Dec, kicks forward one month
 
 
 ################# collapse sample yr/mm to one column ######################
